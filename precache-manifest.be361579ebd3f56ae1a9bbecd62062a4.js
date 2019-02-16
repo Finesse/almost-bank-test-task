3dc3297ba7554f5bd79f35cc76cf467e0656db2a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-16 03:31:03 +0000

    Deploy Finesse/currency-exchange-test-task to github.com/Finesse/currency-exchange-test-task.git:gh-pages

---
+++ precache-manifest.be361579ebd3f56ae1a9bbecd62062a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "66bb45437582ef4b8ba7",
+    "url": "./static/css/main.13c86b2d.chunk.css"
+  },
+  {
+    "revision": "66bb45437582ef4b8ba7",
+    "url": "./static/js/main.66bb4543.chunk.js"
+  },
+  {
+    "revision": "681419391753b86bed32",
+    "url": "./static/js/1.68141939.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "35b05fb9e525fae1b667e1435500d569",
+    "url": "./static/media/arrowRight.35b05fb9.svg"
+  },
+  {
+    "revision": "7ac731f385b1e3d52397a807924e38ba",
+    "url": "./static/media/refresh.7ac731f3.svg"
+  },
+  {
+    "revision": "9b6588d2ca7a87accf6bfbae640a0f63",
+    "url": "./index.html"
+  }
+];
